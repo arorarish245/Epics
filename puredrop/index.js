6f8d801f89759827bebb018eb849677680aaca87
rishav-bhardwaz <rishav76000@gmail.com>
2025-03-28 17:56:59 +0530

    Add StateManager for device state management and integrate filter API

--- puredrop/index.js
+++ puredrop/index.js
@@ -1,2 +1,5 @@
 const PLUGIN_NAME = 'homebridge-puredropgen1'
 const PLATFORM_NAME = 'PureDrops'
+const filterApi = require('./filter/api')
+const path = require('path')
+const storage = require('node-persist')
