022e35aceac1c9fddcc83ae153327ac7ba822e6e
rishav-bhardwaz <rishav76000@gmail.com>
2025-03-31 05:10:47 +0530

    Implement puredropsPlatform class with initialization and error handling for missing refresh token

--- puredrop/index.js
+++ puredrop/index.js
@@ -9,4 +9,32 @@ module.exports = (api) => {
 	api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, puredropsPlatform)
 }
 
+
+class puredropsPlatform {
+	constructor(log, config, api) {
+
+		this.cachedAccessories = []
+		this.activeAccessories = []
+		this.config = config
+		this.log = log
+		this.api = api
+		this.storage = storage
+		this.syncHomeKitCache = syncHomeKitCache(this)
+		this.name = PLATFORM_NAME
+		this.debug = config['debug'] || false
+		this.PLUGIN_NAME = PLUGIN_NAME
+		this.PLATFORM_NAME = PLATFORM_NAME
+
+		
+		this.refreshToken = config['refreshToken']
+		
+		if (!this.refreshToken) {
+			this.log('ERROR \n')
+			this.log('Can\'t start homebridge-puredrops plugin without token !!\n')
+			this.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n')
+			return
+		}
+	}
+}
+
 // file is not completed need time to finish the code
